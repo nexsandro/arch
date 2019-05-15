e33613e6367ad6a4eb8b296faf056c0af185dc65
sebastiao.santos <santos.sandro@gmail.com>
2019-05-15 17:48:41 -0300

    Testes com bootstrap

--- html-view-generator/src/main/java/com/jlabs/view/model/Term.java
+++ html-view-generator/src/main/java/com/jlabs/view/model/Term.java
@@ -9,6 +9,7 @@ public class Term {
     @Id
     @SequenceGenerator(name = "SEQ_TERM_GENERATOR", sequenceName = "SE_TERM", initialValue = 1, allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "SEQ_TERM_GENERATOR")
+    @Column(name = "SQ_TERM", nullable = false)
     private Integer id;
 
     @Column(name = "name", length = 255, nullable = false)
--- model-processor/src/main/java/com/jlabs/processor/HtmlGenerator.java
+++ model-processor/src/main/java/com/jlabs/processor/HtmlGenerator.java
@@ -1,12 +1,16 @@
 package com.jlabs.processor;
 
+import com.jlabs.processor.view.ViewWriter;
+
 import javax.annotation.processing.*;
 import javax.lang.model.SourceVersion;
 import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 import javax.persistence.Entity;
-import javax.tools.Diagnostic;
+import javax.tools.*;
+import java.io.IOException;
+import java.io.Writer;
 import java.util.Set;
 
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
@@ -16,6 +20,7 @@ import java.util.Set;
 public class HtmlGenerator extends AbstractProcessor {
 
     private Messager messager;
+    private ProcessingEnvironment processingEnv;
 
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
@@ -25,6 +30,15 @@ public class HtmlGenerator extends AbstractProcessor {
                 return true;
             }
 
+            try {
+                final FileObject resource = processingEnv.getFiler().createResource(StandardLocation.SOURCE_OUTPUT, "", element.getSimpleName() + ".html", element);
+                final Writer writer = resource.openWriter();
+                new ViewWriter(writer, element).write();
+                writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
         return false;
     }
@@ -32,6 +46,7 @@ public class HtmlGenerator extends AbstractProcessor {
     @Override
     public synchronized void init(ProcessingEnvironment processingEnv) {
         messager = processingEnv.getMessager();
+        this.processingEnv = processingEnv;
         super.init(processingEnv);
     }
 }
